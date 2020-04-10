4127ca4115ef71b344e4e1a0d56ffca3eaee11a0
chy <c990330@126.com>
2020-04-10 17:05:24 +0800

    add for,forEach,Map

--- js/math.js
+++ js/math.js
@@ -8,6 +8,7 @@ function randomArr(arr) {
     console.log(newArr)
 }
 
+// 数组排序
 function sortArr(arr) {
     let newArr = arr.sort(function (a, b) {
         return a - b;
@@ -15,3 +16,35 @@ function sortArr(arr) {
     console.log(newArr)
 }
 
+// 数组对象排序
+function sortObj(arr) {
+    let newArr = arr.sort(function (a, b) {
+        return a.year - b.year;
+    });
+    console.log(newArr)
+}
+
+// 测试for循环，forEach，map 速度
+function getFor(arr) {
+    console.time('for循环:');
+    for (let i = 0; i < arr.length; i++) {
+        const val = arr[i];
+    }
+    console.timeEnd('for循环:');
+}
+
+function getForEach(arr) {
+    console.time('forEach循环:');
+    arr.forEach(e => {
+        const val = e
+    });
+    console.timeEnd('forEach循环:');
+}
+
+function getMap(arr) {
+    console.time('forMap循环:');
+    arr.map(e => {
+        const val = e
+    });
+    console.timeEnd('forMap循环:');
+}
